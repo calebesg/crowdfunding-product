4cac23553ce20c87cb94b818b629777d0b91dc97
CalebeSG <calebeguimaraes10@gmail.com>
2022-04-15 13:53:06 -0300

    created overlay in mobile menu

--- script.js
+++ script.js
@@ -1,11 +1,19 @@
 'use strict';
 
-const nav = document.querySelector('.header__navbar__menu');
-const btnMenu = document.querySelector('.btn__menu');
+// MENU MOBILE
+////////////////////////////////////////////////
+const mobileMenu = function () {
+  const nav = document.querySelector('.header__navbar__menu');
+  const btnMenu = document.querySelector('.btn__menu');
+  const overlay = document.querySelector('.hidden');
 
-const openOrCloseMenu = function () {
-  nav.classList.toggle('show');
-  btnMenu.classList.toggle('close');
+  const openOrCloseMenu = function () {
+    nav.classList.toggle('show');
+    btnMenu.classList.toggle('close');
+    overlay.classList.toggle('hidden');
+  };
+
+  btnMenu.addEventListener('click', openOrCloseMenu);
+  overlay.addEventListener('click', openOrCloseMenu);
 };
-
-btnMenu.addEventListener('click', openOrCloseMenu);
+mobileMenu();
